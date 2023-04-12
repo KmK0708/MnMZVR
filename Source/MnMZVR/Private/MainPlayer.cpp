378d539b7fd9c470052fcc2a194f7dba12ef1801
KmK0708 <kample2k@gmail.com>
2023-04-12 16:49:03 +0900

    4월 12일 수정
    기존 VR 템플릿 IMC가 안먹어서 새로 생성함.
    IMC 수정
    메인플레이어 수정 추가

--- Source/MnMZVR/Private/MainPlayer.cpp
+++ Source/MnMZVR/Private/MainPlayer.cpp
@@ -5,13 +5,50 @@
 #include "EnhancedInputSubsystems.h"
 #include "Engine/LocalPlayer.h"
 #include "EnhancedInputComponent.h"
+#include <Camera/CameraComponent.h>
+#include <MotionControllerComponent.h>
 
 // Sets default values
 AMainPlayer::AMainPlayer()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	// 카메라 설정
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	PlayerCamera->SetupAttachment(RootComponent);
+	PlayerCamera->SetRelativeLocation(FVector(0, 0, 64));
+	PlayerCamera->bUsePawnControlRotation = true;
 
+	// 왼손 추가
+	LeftHand = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("LeftHand"));
+	LeftHand->SetupAttachment(RootComponent);
+	LeftHand->MotionSource = FName("Left");
+	// 오른손
+	RightHand = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("RightHand"));
+	RightHand->SetupAttachment(RootComponent);
+	RightHand->MotionSource = FName("Right");
+
+	// 왼손 스켈레탈 메시 설정
+	LeftHandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("LeftHandMesh"));
+	// 스켈레탈 메시 로드 후 할당
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> LHandMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/MannequinsXR/Meshes/SKM_MannyXR_left.SKM_MannyXR_left'"));
+	if (LHandMesh.Succeeded())
+	{
+		LeftHandMesh->SetSkeletalMesh(LHandMesh.Object);
+		// (X=-2.981260,Y=-3.500000,Z=4.561753)
+		// (Pitch=-25.000000,Yaw=-179.999999,Roll=89.999998)
+	}
+
+	// 오른손 스켈레탈 메시 설정
+	RightHandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("RightHandMesh"));
+	// 스켈레탈 메시 로드 후 할당
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> RHandMesh(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/MannequinsXR/Meshes/SKM_MannyXR_right.SKM_MannyXR_right'"));
+	if (RHandMesh.Succeeded())
+	{
+		RightHandMesh->SetSkeletalMesh(RHandMesh.Object);
+		// (X=-2.981260,Y=3.500000,Z=4.561753)
+		// (Pitch=25.000000,Yaw=0.000000,Roll=89.999999)
+	}
 }
 
 // Called when the game starts or when spawned
@@ -29,7 +66,7 @@ void AMainPlayer::BeginPlay()
 		auto subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(localPlayer);
 		if (subSystem)
 		{
-			subSystem->AddMappingContext(IMC_Defaults, 0);
+			subSystem->AddMappingContext(IMC_PlayerInput, 0);
 		}
 	}
 }
@@ -50,6 +87,7 @@ void AMainPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	if (enhancedInputComponent)
 	{
 		enhancedInputComponent->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AMainPlayer::Move);
+		enhancedInputComponent->BindAction(IA_Mouse, ETriggerEvent::Triggered, this, &AMainPlayer::Turn);
 	}
 }
 
@@ -58,14 +96,23 @@ void AMainPlayer::Move(const FInputActionValue& Values)
 	// 사용자의 입력에따라 앞뒤좌우로 이동하고 싶다.
 	// 1. 사용자의 입력에 따라
 	FVector2D Axis = Values.Get<FVector2D>();
-	// 2. 앞뒤좌우라는 방향이 필요.
-	FVector Dir(Axis.X, Axis.Y, 0);
-	// 3. 이동하고싶다.
-	// P = P0 + vt
-	FVector P0 = GetActorLocation();
-	FVector vt = Dir * PlayerSpeed * GetWorld()->DeltaTimeSeconds;
-	FVector P = P0 + vt;
 	UE_LOG(LogTemp, Warning, TEXT("move"));
-	SetActorLocation(P);
+	AddMovementInput(GetActorForwardVector(), Axis.X);
+	AddMovementInput(GetActorRightVector(), Axis.Y);
+	// 2. 앞뒤좌우라는 방향이 필요.
+// 	FVector Dir(Axis.X, Axis.Y, 0);
+// 	// 3. 이동하고싶다.
+// 	// P = P0 + vt
+// 	FVector P0 = GetActorLocation();
+// 	FVector vt = Dir * PlayerSpeed * GetWorld()->DeltaTimeSeconds;
+// 	FVector P = P0 + vt;
+// 	SetActorLocation(P);
+}
+
+void AMainPlayer::Turn(const FInputActionValue& Values)
+{
+	FVector2D Axis = Values.Get<FVector2D>();
+	AddControllerYawInput(Axis.X);
+	AddControllerPitchInput(Axis.Y);
 }
 
--- Source/MnMZVR/Public/MainPlayer.h
+++ Source/MnMZVR/Public/MainPlayer.h
@@ -33,11 +33,39 @@ public:
 	float PlayerSpeed = 600.0f;
 	// 플레이어 인풋 맵핑컨텍스트
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
-	class UInputMappingContext* IMC_Defaults;
+	class UInputMappingContext* IMC_PlayerInput;
 	// 인풋 액션
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
 	class UInputAction* IA_Move;
+	// 플레이어 마우스 입력
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	class UInputAction* IA_Mouse;
+
+	//===========플레이어 손===========//
+	// 왼손
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Hand")
+	class UMotionControllerComponent* LeftHand;
+	// 오른손
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Hand")
+	class UMotionControllerComponent* RightHand;
+	// 왼손 모델
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Hand")
+	class USkeletalMeshComponent* LeftHandMesh;
+	// 오른손 모델
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Hand")
+	class USkeletalMeshComponent* RightHandMesh;
+
+	//=================================//
+
 
 	// 이동처리함수
 	void Move(const FInputActionValue& Values);
+	// 회전처리 함수
+	void Turn(const FInputActionValue& Values);
+
+
+protected:
+// 플레이어 카메라
+UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "VRCamera")
+	class UCameraComponent* PlayerCamera;
 };
