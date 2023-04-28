72cc929e97f08fabc5f5c3b6c90822a0053df364
KmK0708 <kample2k@gmail.com>
2023-04-28 16:57:06 +0900

    Revert "Revert "4월 27일""
    This reverts commit c704724313bb4b65b792e597ebfbb9eef7fd099b.

---
+++ Source/MnMZVR/Private/ItemBase.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemBase.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "TestEnemy.h"
+#include "Enemy_Skeleton.h"
+#include "MainPlayer.h"
+
+
+// Sets default values
+AItemBase::AItemBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AItemBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AItemBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AItemBase::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
---
+++ Source/MnMZVR/Public/InventorySystem.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ItemBase.h"
+#include "InventorySystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MNMZVR_API UInventorySystem : public UObject
+{
+	GENERATED_BODY()
+	
+};
